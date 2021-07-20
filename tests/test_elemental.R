78805cdfa50e7141d4e77807c18270b44204a206
marberts <stevemartin041@gmail.com>
2021-07-20 15:54:22 -0400

    Update to work with gpindex >= 0.3.3

--- tests/test_elemental.R
+++ tests/test_elemental.R
@@ -32,9 +32,15 @@ all.equal(epr1$index, lapply(epr1$contributions, function(x) sapply(x, sum) + 1)
 all.equal(epr2$index, lapply(epr2$contributions, function(x) sapply(x, sum, na.rm = TRUE) + 1))
 all.equal(epr3$index, lapply(epr3$contributions, function(x) sapply(x, sum, na.rm = TRUE) + 1))
 
+#---- Superlative epr ----
 l <- with(dat, elemental_index(rel, period, ea, w1, r = 1, na.rm = TRUE))
 p <- with(dat, elemental_index(rel, period, ea, w2, r = -1, na.rm = TRUE))
 all.equal(sqrt(as.matrix(l) * as.matrix(p)), as.matrix(epr3))
+
+sepr <- with(dat, superlative_elemental_index(rel, period, ea, w2 = w2, na.rm = TRUE, s = 3))
+l <- with(dat, elemental_index(rel, period, ea, r = 1.5, na.rm = TRUE))
+p <- with(dat, elemental_index(rel, period, ea, w2, r = -1.5, na.rm = TRUE))
+all.equal(sqrt(as.matrix(l) * as.matrix(p)), as.matrix(sepr))
 
 #---- Merging ----
 epr3 <- merge(epr1, epr2)
