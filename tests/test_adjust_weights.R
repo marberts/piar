f95261befc9a845e2d57907268c70e5b9ea4fe86
marberts <stevemartin041@gmail.com>
2021-09-21 16:23:37 -0400

    Added tests for adjust()

---
+++ tests/test_adjust_weights.R
@@ -0,0 +1,11 @@
+library(piar)
+
+# Reproduce example for CPI
+rel <- c(1.1, 0.82, 1.33, 1, 1, 1.08, 0.77, 0.9, 1.11, 1.1, 0.95, 1.05)
+epr <- elemental_index(rel, period = 1:12)
+pias <- aggregation_structure(list("b", 1), 155)
+weights(adjust(pias, aggregate(epr, pias)))
+
+# Do it again with a fixed-base index
+epr2 <- elemental_index(cumprod(rel), period = 1:12)
+weights(adjust(pias, aggregate(epr, pias, chained = FALSE)))
