e2a2863d13bd9a759505e09726235f65541c7a65
Steve Martin <stevemartin041@gmail.com>
2023-07-10 00:40:31 -0400

    Moved some more tests over

--- tests/testthat/test-aggregation_structure.R
+++ tests/testthat/test-aggregation_structure.R
@@ -1,5 +1,5 @@
-x1 <- c(  "1",   "2",   "1",   "1")
-x2 <- c( "11",  "21",  "12",  "11")
+x1 <- c("1", "2", "1", "1")
+x2 <- c("11", "21", "12", "11")
 x3 <- c("111", "211", "121", "112")
 agg <- aggregation_structure(list(x1, x2, x3))
 
@@ -8,7 +8,7 @@ test_that("coercion methods works", {
     as.matrix(agg),
     matrix(c(2, 0, 3, 0, 0, 0, 6, 0, 0, 6, 2, 0, 0, 6, 0, 2, 0, 3, 0, 0) / 6,
            5, 4,
-           dimnames = list(c("1", "2", "11", "12", "21"), 
+           dimnames = list(c("1", "2", "11", "12", "21"),
                            c("111", "211", "121", "112")))
   )
   expect_equal(
@@ -19,12 +19,15 @@ test_that("coercion methods works", {
                weight = c(1, 1, 1, 1))
   )
   expect_equal(agg, as_aggregation_structure(as.data.frame(agg)))
-  
+  expect_equal(as.list(agg), list(x1, x2, x3))
+
   expect_equal(as.matrix(aggregation_structure(1:3, 2)),
                matrix(c(1, 1), 2, 1, dimnames = list(c("1", "2"), "3")))
   expect_equal(as.data.frame(aggregation_structure(1:3, 2)),
                data.frame(level1 = "1", level2 = "2", ea = "3", weight = 2))
-  
+  expect_equal(as.list(aggregation_structure(1:3, 2)),
+               as.list(as.character(1:3)))
+
   expect_equal(
     as.matrix(aggregation_structure(list(letters[c(2, 1, 3)]), 1:3)),
     matrix(numeric(0), 0, 3, dimnames = list(character(0), c("b", "a", "c")))
@@ -32,6 +35,10 @@ test_that("coercion methods works", {
   expect_equal(
     as.data.frame(aggregation_structure(list(letters[c(2, 1, 3)]), 1:3)),
     data.frame(ea = c("b", "a", "c"), weight = 1:3)
+  )
+  expect_equal(
+    as.list(aggregation_structure(list(letters[c(2, 1, 3)]), 1:3)),
+    list(letters[c(2, 1, 3)])
   )
 })
 
@@ -46,12 +53,12 @@ test_that("weights method works", {
                list(c("1" = 8, "2" = 2),
                     c("11" = 5, "12" = 3, "21" = 2),
                     c("111" = 1, "211" = 2, "121" = 3, "112" = 4)))
-  
+
   expect_equal(weights(aggregation_structure(list(x1, x2, x3), c(NA, 2:4))),
                list(c("1" = NA, "2" = 2),
                     c("11" = NA, "12" = 3, "21" = 2),
                     c("111" = NA, "211" = 2, "121" = 3, "112" = 4)))
-  
+
   expect_equal(
     weights(aggregation_structure(list(x1, x2, x3), c(NA, 2:4)), na.rm = TRUE),
     list(c("1" = 7, "2" = 2),
@@ -65,12 +72,12 @@ test_that("updating works", {
   epr <- elemental_index(integer(0))
   index <- aggregate(epr, agg)
   expect_equal(update(agg, index)[-5], agg[-5])
-  
+
   expect_equal(weights(update(agg, index)),
                list(c("1" = NA_real_, "2" = NA),
                     c("11" = NA_real_, "12" = NA, "21" = NA),
                     c("111" = NA_real_, "211" = NA, "121" = NA, "112" = NA)))
-  
+
   # Updating with an index that doesn't line up with the pias introduces NA
   # weights that should carry up the aggregation structure
   epr <- elemental_index(1, ea = "111")
@@ -82,10 +89,29 @@ test_that("updating works", {
 })
 
 test_that("reordering works", {
-  epr <- as_index(matrix(1:12, 4, 3, dimnames = list(c("111", "112", "121", "211"), 1:3)))
+  epr <- as_index(matrix(1:12, 4, 3,
+                         dimnames = list(c("111", "112", "121", "211"), 1:3)))
   agg1 <- aggregation_structure(list(x1, x2, x3), 1:4)
-  agg2 <- aggregation_structure(list(x1, factor(x2, levels = c("11", "21", "12")), x3), 1:4)
+  f <- factor(x2, levels = c("11", "21", "12"))
+  agg2 <- aggregation_structure(list(x1, f, x3), 1:4)
   index1 <- aggregate(epr, agg1)
   index2 <- aggregate(epr, agg2)
   expect_equal(as.matrix(index1[levels(index2)]), as.matrix(index2))
+})
+
+test_that("as_aggregation_structure methods work", {
+  expect_equal(agg, as_aggregation_structure(data.frame(x1, x2, x3, 1)))
+  expect_equal(agg, as_aggregation_structure(cbind(x1, x2, x3, 1)))
+  epr <- elemental_index(1, ea = "111")
+  index <- aggregate(epr, agg)
+  expect_equal(agg, as_aggregation_structure(index))
+})
+
+test_that("errors work", {
+  expect_error(aggregation_structure(list()))
+  expect_error(aggregation_structure(list(x1, x2, x3), 1:3))
+  expect_error(aggregation_structure(list(x1, x2, x3, x3)))
+  expect_error(aggregation_structure(list(x1, x2)))
+  expect_error(aggregation_structure(list(1:2, c(11, NA))))
+  expect_warning(aggregation_structure(list(1:2, c(3, 3), c(4, 5))))
 })
---
+++ tests/testthat/test-vcov.R
@@ -0,0 +1,126 @@
+library(gpindex)
+
+set.seed(4321)
+
+prices <- data.frame(rel = runif(24), 
+                     period = 1:3, 
+                     id = rep(letters[1:8], each = 3))
+
+weights <- data.frame(l1 = rep(1, 8), 
+                      l2 = rep(c(11, 12), each = 4), 
+                      l3 = rep(c(111, 112, 121, 122), each = 2),
+                      l4 = letters[1:8],
+                      ew = round(1000 * runif(8)),
+                      dw = c(1, runif(6, 1, 10), 1))
+
+epr <- with(prices, elemental_index(rel, period, id))
+pias <- with(weights, aggregation_structure(as.list(weights[1:4]), ew * dw))
+index <- aggregate(epr, pias)
+
+rw <- matrix(runif(8 * 25), 8)
+
+test_that("corner cases work", {
+  expect_equal(
+    vcov(aggregate(elemental_index(numeric(0)), aggregation_structure("a")),
+         matrix(1:10, ncol = 10)),
+    matrix(numeric(0), 0, 1, dimnames = list(NULL, "1"))
+  )
+  
+  expect_equal(
+    vcov(aggregate(elemental_index(numeric(0)), aggregation_structure(1:5)),
+         matrix(runif(10), ncol = 10)),
+    matrix(NA_real_, 4, 1, dimnames = list(1:4, 1))
+  )
+})
+
+test_that("mse == FALSE works", {
+  covar <- vcov(index, rw * weights$ew, mse = FALSE)
+  
+  # Variance for higher levels should agree with manual calculation
+  # Period 1
+  rws <- apply(rw * weights$ew, 2, scale_weights)
+  expect_equal(
+    sum(
+      crossprod(
+        as.matrix(epr[, 1]),
+        tcrossprod(sweep(rws, 1, rowMeans(rws))) / 25) * t(as.matrix(epr[, 1]))
+      ),
+    covar[1, 1] # 0.004973962 according to svymean
+  )
+  # Period 2
+  rws <- apply(rw * weights(update(pias, index, 1), TRUE) / weights$dw, 2,
+               scale_weights)
+  expect_equal(
+    sum(
+      crossprod(
+        as.matrix(epr[, 2]),
+        tcrossprod(sweep(rws, 1, rowMeans(rws))) / 25) * t(as.matrix(epr[, 2]))
+      ),
+    covar[1, 2] # 0.003076949 according to svymean
+  )
+})
+
+test_that("mse == TRUE case works", {
+  covar <- vcov(index, rw * weights$ew)
+  
+  # Variance for higher levels should agree with manual calculation
+  # Period 1
+  rws <- apply(rw * weights$ew, 2, scale_weights)
+  w <- weights(pias, ea_only = TRUE)
+  expect_equal(
+    sum(
+      crossprod(
+        as.matrix(epr[, 1]),
+        tcrossprod(sweep(rws, 1, scale_weights(w))) / 25) *
+          t(as.matrix(epr[, 1]))
+      ),
+    covar[1, 1] # 0.02001844 according to svymean
+  )
+  
+  # Period 2
+  rws <- apply(rw * weights(update(pias, index, 1), TRUE) / weights$dw, 2,
+               scale_weights)
+  expect_equal(
+    sum(
+      crossprod(
+        as.matrix(epr[, 2]),
+        tcrossprod(
+          sweep(
+            rws, 1,
+            scale_weights(weights(update(pias, index, 1), TRUE)))
+          ) / 25
+        ) * t(as.matrix(epr[, 2]))
+      ),
+    covar[1, 2] # 0.008153173 according to svymean
+  )
+})
+
+test_that("vcov works for chained index", {
+  covar <- vcov(chain(index), rw * weights$ew)
+  
+  # Variance for higher levels should agree with manual calculation
+  # Period 1
+  rws <- apply(rw * weights$ew, 2, scale_weights)
+  w <- weights(pias, ea_only = TRUE)
+  expect_equal(
+    sum(
+      crossprod(
+        as.matrix(epr[, 1]),
+        tcrossprod(sweep(rws, 1, scale_weights(w))) / 25) *
+          t(as.matrix(epr[, 1]))
+    ),
+    covar[1, 1] # 0.02001844 according to svymean
+  )
+  
+  # Period 2
+  epr <- chain(epr)
+  expect_equal(
+    sum(
+      crossprod(
+        as.matrix(epr[, 2]),
+        tcrossprod(sweep(rws, 1, scale_weights(w))) / 25) *
+          t(as.matrix(epr[, 2]))
+    ),
+    covar[1, 2] # 0.004125957 according to svymean
+  )
+})
